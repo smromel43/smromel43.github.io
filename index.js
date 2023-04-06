a443f0b65a5dfcc5906938484a53172026625e02
Md Shaklian Mostak Romel <129885577+smromel43@users.noreply.github.com>
2023-04-06 23:02:07 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["Engineer", "E-sports Player", "Web Developer", "Volleyball Player"],
+    typeSpeed: 100,
+    bsckSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Engineer.", "E-sportss Player.", "Web Developer.", "Volleyball Player.", "Designer."],
+    typeSpeed: 100,
+    bsckSpeed: 60,
+    loop: true
+});
